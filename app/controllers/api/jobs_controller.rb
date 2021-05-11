6668b5191b83ad50906ce6e5813f097415fafced
TIMM SNOW <two.tone.snow@gmail.com>
2021-05-11 17:12:46 -0400

    creates RESTFUL API for JOB model

---
+++ app/controllers/api/jobs_controller.rb
@@ -0,0 +1,61 @@
+class Api::JobsController < ApplicationController
+  def index
+    @jobs = Job.where(user_id: current_user.id)
+    render "index.json.jb"
+  end
+
+  def show
+    @job = Job.find(params[:id])
+    render "show.json.jb"
+  end
+
+  def create
+    job = Job.new(
+      company_name: params[:company_name],
+      position: params[:position],
+      description: params[:description],
+      status: params[:status],
+      salary: params[:salary],
+      posting_url: params[:posting_url],
+      notes: params[:notes],
+      date_updated: params[:date_updated],
+      user_id: current_user.id,
+    )
+    if job.save
+      render json: { message: "job created successfully" }, status: :created
+    else
+      render json: { errors: job.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def update
+    @job = Job.find(params[:id])
+
+    if current_user.id == @job.user_id
+      @job.company_name = params[:company_name] || @job.company_name
+      @job.position = params[:position] || @job.position
+      @job.description = params[:description] || @job.description
+      @job.status = params[:status] || @job.status
+      @job.salary = params[:salary] || @job.salary
+      @job.posting_url = params[:posting_url] || @job.posting_url
+      @job.notes = params[:notes] || @job.notes
+      @job.date_updated = params[:date_updated] || @job.date_updated
+      #happy/sad path
+      if @job.save
+        render json: { message: "you did it" }
+      else
+        render json: { error: @job.errors.full_messages }, status: 400
+      end
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    job_id = params[:id]
+    if current_user.id == @job.user_id
+      @job = Job.find(job_id)
+      @job.destroy
+      render json: { message: "job DESTROYED" }
+    end
+  end
+end
