72bb0f6de9fd586d18bb23388255571fa916314b
TIMM SNOW <two.tone.snow@gmail.com>
2021-07-13 08:35:07 -0400

    debugging deployment issue with root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
-
+  root 'users#index'
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     #user routes
